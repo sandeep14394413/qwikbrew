27f8073f612d891c919c9a861e3242d7b5f926df
sandeep14394413 <89721933+sandeep14394413@users.noreply.github.com>
2026-03-20 13:05:06 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,7 +6,10 @@ variable "aiven_api_token"     { sensitive = true }
 variable "aiven_project"       {}
 variable "aiven_cloud_region"  { default = "aws-ap-south-1" }
 
-variable "payment_gateway_key_id" { default = ""; sensitive = true }
+variable "payment_gateway_key_id" {
+  default   = ""
+  sensitive = true
+}
 variable "payment_gateway_secret" { default = ""; sensitive = true }
 variable "fcm_server_key"         { default = ""; sensitive = true }
 variable "smtp_host"              { default = "email-smtp.ap-south-1.amazonaws.com" }
