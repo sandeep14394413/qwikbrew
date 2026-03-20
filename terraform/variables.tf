80cc725e9d2904f51bcf6c2e5855bf6a4e1ed9c6
sandeep14394413 <89721933+sandeep14394413@users.noreply.github.com>
2026-03-20 13:08:41 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,7 +10,10 @@ variable "payment_gateway_key_id" {
   default   = ""
   sensitive = true
 }
-variable "payment_gateway_secret" { default = ""; sensitive = true }
+variable "payment_gateway_secret" { 
+  default = "" 
+  sensitive = true 
+}
 variable "fcm_server_key"         { default = ""; sensitive = true }
 variable "smtp_host"              { default = "email-smtp.ap-south-1.amazonaws.com" }
 variable "smtp_user"              { default = ""; sensitive = true }
